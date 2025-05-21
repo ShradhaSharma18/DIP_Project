6f9f84f8f89a6bce4ce45251ee61a084160357eb
ShradhaSharma18 <shradhasharma590@gmail.com>
2025-05-21 23:14:28 +0530

    Initial commit for DIP Project

---
+++ utils/constants.py
@@ -0,0 +1,21 @@
+# Window size
+WIDTH, HEIGHT = 700, 500
+
+# Ball Variables
+BALL_RADIUS = 15
+BALL_COLOR = (255, 255, 255)  # White color in RGB
+BALL_VEL = 2  # Initial velocity of the ball
+
+# Paddle Variables
+PADDLE_WIDTH = 20
+PADDLE_HEIGHT = 100
+
+# Padding for paddles
+LEFT_OFFSET = PADDLE_WIDTH  # Distance from the left edge
+RIGHT_OFFSET = WIDTH - LEFT_OFFSET  # Distance from the right edge
+
+# Speed increment factor
+SPEED_INCREMENT = 1.25  # Factor by which the ball's speed increases
+
+# Score to win
+MAX_SCORE = 5
